0a40ab996caac911db7489fcb26c0b930146e39c
dxxmwrld <clavka40rus1@mail.ru>
2022-11-22 11:47:25 +0300

    task5-fix

--- task5/task5.py
+++ task5/task5.py
@@ -1,13 +1,14 @@
+import json
 import numpy as np
 
 
 def mas(A):
-  numb=[]
+  numb = []
   for item in A:
-    try:    
+    try:
       numb.append(int(item))
     except:
-      mas=[]
+      mas = []
       for i in range(len(item)):
         mas.append(int(item[i]))
       numb.append(mas)
@@ -15,51 +16,54 @@ def mas(A):
 
 
 def get_table(numb):
-  max=0
+  max = 0
   for elem in numb:
     try:
-      if elem>max:
-        max=elem
+      if elem > max:
+        max = elem
     except:
       for i in elem:
-        if i>max:
-          max=i
-  table = np.zeros((max,max)) 
-  mas=np.zeros(max)
+        if i > max:
+          max = i
+  table = np.zeros((max, max))
+  mas = np.zeros(max)
   for item in numb:
     try:
-      mas[item-1]=1
+      mas[item - 1] = 1
       for j in range(max):
-        if mas[j]==0:
-          table[j,item-1]=0
+        if mas[j] == 0:
+          table[j, item - 1] = 0
         else:
-          table[j,item-1]=1
+          table[j, item - 1] = 1
     except:
       for elem in item:
-        mas[elem-1]=1
+        mas[elem - 1] = 1
       for elem in item:
         for j in range(max):
-          if mas[j]==0:
-            table[j,elem-1]=0
+          if mas[j] == 0:
+            table[j, elem - 1] = 0
           else:
-            table[j,elem-1]=1
+            table[j, elem - 1] = 1
   return table
 
 
 def back_string(arg):
-  estimated=[]
+  estimated = []
   for item in arg:
     try:
-      estimated.append(str(item+1))
+      estimated.append(str(item + 1))
     except:
-      mas=[]
+      mas = []
       for elem in item:
-        mas.append(str(elem+1))
+        mas.append(str(elem + 1))
       estimated.append(mas)
   return estimated
 
 
-def task(dataA,dataB):
+def task(str1,str2):
+  dataA = json.loads(str1)
+  dataB = json.loads(str2)
+
   dataA=mas(dataA)
   dataB=mas(dataB)
   tableA=get_table(dataA)
